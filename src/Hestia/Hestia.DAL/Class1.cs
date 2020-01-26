c5cdada1894e12abef72b247028c59e27b475022
David Marczinus <marczinus.david@gmail.com>
2020-01-26 12:21:17 +0100

    Add sqlite DAL project

---
+++ src/Hestia/Hestia.DAL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hestia.DAL
+{
+    /// <summary>
+    /// Class1.
+    /// </summary>
+    // ReSharper disable once UnusedType.Global
+    public class Class1 : DbContext
+    {
+        /// <inheritdoc/>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("todo");
+        }
+    }
+}
