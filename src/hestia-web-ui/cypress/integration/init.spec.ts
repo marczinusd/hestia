3219cad97dd7a503a76401831939e136c24887c9
David Marczinus <marczinus.david@gmail.com>
2020-02-08 18:02:19 +0100

    [Build] Move UI source to Hestia.UI project

--- src/hestia-web-ui/cypress/integration/init.spec.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-it("visits the app", () => {
-    cy.visit("/");
-});
-
-it("has element that contains 'Learn React'", () => {
-    cy.visit("").contains<HTMLAnchorElement>("Learn React");
-});
--- src/hestia-web-ui/src/apis/dummy.ts
+++
@@ -1,5 +0,0 @@
-import Axios from "axios";
-
-export const dummy = Axios.create({
-  baseURL: ""
-});
