a2aa4b4db62dc2aeff797f577188255c24554536
David Marczinus <marczinus.david@gmail.com>
2020-03-08 23:00:52 +0100

    [test] add test for PathValidator

--- src/Test.Hestia.Model/Builders/PathValidatorTest.cs
+++ src/Test.Hestia.Model/Builders/PathValidatorTest.cs
@@ -1,13 +1,43 @@
+using System;
+using FluentAssertions;
+using Hestia.Model.Builders;
 using Xunit;
 
 namespace Test.Hestia.Model.Builders
 {
     public class PathValidatorTest
     {
-        // TODO
         [Fact]
-        public void TODO()
+        public void ValidatorShouldThrowInvalidArgumentExceptionForNullParameter()
         {
+            var validator = new PathValidator();
+
+            Action act = () => validator.ValidateDirectoryPath(null);
+
+            act.Should()
+               .Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ValidatorShouldThrowInvalidArgumentExceptionForOnlyWhitespaceParameter()
+        {
+            var validator = new PathValidator();
+
+            Action act = () => validator.ValidateDirectoryPath("     ");
+
+            act.Should()
+               .Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void ValidatorShouldThrowInvalidOperationExceptionIfDirectoryProvidedDoesNotExist()
+        {
+            var validator = new PathValidator();
+
+            Action act = () => validator.ValidateDirectoryPath("/bla/bla");
+
+            act.Should()
+               .Throw<InvalidOperationException>();
         }
     }
 }
