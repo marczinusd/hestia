e55f3718d0f63f5d51d6db6f1165c4e850d3b48c
David Marczinus <marczinus.david@gmail.com>
2020-03-08 22:57:47 +0100

    [test] add new test for File.FullPath

--- src/Test.Hestia.Model/FileTest.cs
+++ src/Test.Hestia.Model/FileTest.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using FluentAssertions;
 using Hestia.Model;
 using Hestia.Model.Stats;
 using LanguageExt;
 using Xunit;
+using File = Hestia.Model.File;
 
 namespace Test.Hestia.Model
 {
@@ -13,9 +15,9 @@ namespace Test.Hestia.Model
         [Fact]
         public void WhenCreatesANewFileObject()
         {
-            var file = new File(String.Empty,
-                                String.Empty,
-                                String.Empty,
+            var file = new File(string.Empty,
+                                string.Empty,
+                                string.Empty,
                                 new List<SourceLine>(),
                                 Option<FileGitStats>.None,
                                 Option<FileCoverageStats>.None);
@@ -27,9 +29,9 @@ namespace Test.Hestia.Model
         [Fact]
         public void WhenShouldCreateInstanceWithProvidedParams()
         {
-            var file = new File(String.Empty,
-                                String.Empty,
-                                String.Empty,
+            var file = new File(string.Empty,
+                                string.Empty,
+                                string.Empty,
                                 new List<SourceLine>(),
                                 Option<FileGitStats>.None,
                                 Option<FileCoverageStats>.None);
@@ -56,6 +58,60 @@ namespace Test.Hestia.Model
                                         () => null)
                    .Should()
                    .NotBeNull();
+        }
+
+        [Fact]
+        public void AsFileDetailsCreatesEquivalentFileDetailsObject()
+        {
+            var file = new File(string.Empty,
+                                string.Empty,
+                                string.Empty,
+                                new List<SourceLine>
+                                {
+                                    new SourceLine(1,
+                                                   string.Empty,
+                                                   Option<LineCoverageStats>.None,
+                                                   Option<LineGitStats>.None),
+                                },
+                                new FileGitStats(1, 1),
+                                new FileCoverageStats(new FileCoverage(string.Empty,
+                                                                       new List<(int lineNumber, int hitCount)
+                                                                       >())));
+            var fileDetails = file.AsFileDetails();
+
+            fileDetails.Content.Should()
+                       .BeEquivalentTo(file.Content);
+            fileDetails.Extension.Should()
+                       .BeEquivalentTo(file.Extension);
+            fileDetails.Filename.Should()
+                       .BeEquivalentTo(file.Filename);
+            fileDetails.Path.Should()
+                       .BeEquivalentTo(file.Path);
+            fileDetails.CoverageStats
+                       .Match(x => x.Coverage.FileName,
+                              () => null)
+                       .Should()
+                       .BeEmpty();
+            fileDetails.GitStats
+                       .Match(x => x.LifetimeAuthors,
+                              () => 0)
+                       .Should()
+                       .Be(1);
+        }
+
+        [Fact]
+        public void FullPathCorrectlyAssemblesFullFilePath()
+        {
+            var file = new File("some.cs",
+                                string.Empty,
+                                "dir",
+                                new List<SourceLine>(),
+                                Option<FileGitStats>.None,
+                                Option<FileCoverageStats>.None);
+
+            file.FullPath
+                .Should()
+                .BeEquivalentTo($"{file.Path}{Path.DirectorySeparatorChar}{file.Filename}");
         }
     }
 }
