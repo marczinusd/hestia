6595c21fb59386bcad5208a0ce69810fa6535562
David Marczinus <marczinus.david@gmail.com>
2020-03-08 22:47:33 +0100

    [test] add tests for File

--- src/Test.Hestia.Model/FileTest.cs
+++ src/Test.Hestia.Model/FileTest.cs
@@ -1,13 +1,61 @@
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Hestia.Model;
+using Hestia.Model.Stats;
+using LanguageExt;
 using Xunit;
 
 namespace Test.Hestia.Model
 {
     public class FileTest
     {
-        // TODO
         [Fact]
-        public void TODO()
+        public void WhenCreatesANewFileObject()
         {
+            var file = new File(String.Empty,
+                                String.Empty,
+                                String.Empty,
+                                new List<SourceLine>(),
+                                Option<FileGitStats>.None,
+                                Option<FileCoverageStats>.None);
+
+            file.Should()
+                .NotBeSameAs(file.With());
+        }
+
+        [Fact]
+        public void WhenShouldCreateInstanceWithProvidedParams()
+        {
+            var file = new File(String.Empty,
+                                String.Empty,
+                                String.Empty,
+                                new List<SourceLine>(),
+                                Option<FileGitStats>.None,
+                                Option<FileCoverageStats>.None);
+
+            var newFile = file.With(new List<SourceLine>
+                                    {
+                                        new SourceLine(1,
+                                                       string.Empty,
+                                                       Option<LineCoverageStats>.None,
+                                                       Option<LineGitStats>.None),
+                                    },
+                                    new FileGitStats(1, 1),
+                                    new FileCoverageStats(new FileCoverage(string.Empty,
+                                                                           new List<(int lineNumber, int hitCount)
+                                                                           >())));
+
+            newFile.Content.Should()
+                   .HaveCount(1);
+            newFile.GitStats.Match(x => x.LifetimeAuthors,
+                                   () => 0)
+                   .Should()
+                   .Be(1);
+            newFile.CoverageStats.Match(x => x.Coverage,
+                                        () => null)
+                   .Should()
+                   .NotBeNull();
         }
     }
 }
