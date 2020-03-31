6ec6e1a8c9b14c0e5e782c7a5ce4db0fd06d45ac
David Marczinus <marczinus.david@gmail.com>
2020-03-31 21:46:37 +0200

    feat: wire up autofac container

--- src/Hestia.UIRunner/App.xaml.cs
+++ src/Hestia.UIRunner/App.xaml.cs
@@ -1,9 +1,15 @@
 using System.Diagnostics.CodeAnalysis;
+using Autofac;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Hestia.Model.Builders;
+using Hestia.Model.Stats;
+using Hestia.Model.Wrappers;
 using Hestia.UIRunner.ViewModels;
 using Hestia.UIRunner.Views;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Hestia.UIRunner
 {
@@ -19,13 +25,37 @@ namespace Hestia.UIRunner
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                var container = BuildContainer();
+                desktop.MainWindow = new MainWindow { DataContext = container.Resolve<MainWindowViewModel>(), };
             }
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindowViewModel>();
+            builder.RegisterType<DiskIOWrapper>()
+                   .As<IDiskIOWrapper>();
+            builder.RegisterType<GitCommands>()
+                   .As<IGitCommands>();
+            builder.RegisterType<CommandLineExecutor>()
+                   .As<ICommandLineExecutor>();
+            builder.RegisterType<CoverageProviderFactory>()
+                   .As<ICoverageProviderFactory>();
+            builder.RegisterType<PathValidator>()
+                   .As<IPathValidator>();
+            builder.RegisterType<ReportGeneratorWrapper>()
+                   .As<IReportGeneratorWrapper>();
+            builder.RegisterType<CoverageReportConverter>()
+                   .As<ICoverageReportConverter>();
+            builder.RegisterInstance<ILogger<IStatsEnricher>>(new NullLogger<IStatsEnricher>());
+            builder.RegisterType<StatsEnricher>()
+                   .As<IStatsEnricher>();
+
+            return builder.Build();
         }
     }
 }
