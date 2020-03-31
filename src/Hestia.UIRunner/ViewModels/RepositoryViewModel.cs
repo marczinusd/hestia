51857ccc365a85c24aa74a0c720fdf7c7b900404
David Marczinus <marczinus.david@gmail.com>
2020-03-31 20:17:34 +0200

    fix: fixed datagrid by adding the style to app.xaml

--- src/Hestia.UIRunner/ViewModels/RepositoryViewModel.cs
+++ src/Hestia.UIRunner/ViewModels/RepositoryViewModel.cs
@@ -1,7 +1,37 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Hestia.Model;
+using Hestia.Model.Stats;
+using LanguageExt;
+
 namespace Hestia.UIRunner.ViewModels
 {
     public class RepositoryViewModel : ViewModelBase
     {
+        public RepositoryViewModel()
+        {
+            Files = new ObservableCollection<File>(GenerateMockPeopleTable());
+        }
+
         public string Text => "Repository view works";
+
+        public ObservableCollection<File> Files { get; set; }
+
+        private IEnumerable<File> GenerateMockPeopleTable()
+        {
+            var defaultPeople = new List<File>()
+            {
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+                new File("bla", "cs", "cs", new List<SourceLine>(), Option<FileGitStats>.None, Option<FileCoverageStats>.None),
+            };
+
+            return defaultPeople;
+        }
     }
 }
