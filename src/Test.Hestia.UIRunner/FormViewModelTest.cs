a11a9165309f12ea1f0118eff9293d240952d79e
David Marczinus <marczinus.david@gmail.com>
2020-04-20 20:11:52 +0200

    chore: remove redundant using

--- src/Test.Hestia.UIRunner/FormViewModelTest.cs
+++ src/Test.Hestia.UIRunner/FormViewModelTest.cs
@@ -11,7 +11,6 @@ using Hestia.Model.Wrappers;
 using Hestia.UIRunner.Services;
 using Hestia.UIRunner.ViewModels;
 using LanguageExt;
-using Metsys.Bson;
 using Microsoft.Reactive.Testing;
 using Moq;
 using Test.Hestia.Utils;
