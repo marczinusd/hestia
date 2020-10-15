f3e62ca2af07dfcb01d08000e0d2b78239ee248b
David Marczinus <marczinus.david@gmail.com>
2020-10-15 12:16:31 +0200

    test: cover SlimSnapshot

---
+++ src/Test.Hestia.WebService/ServiceModel/SlimSnapshotTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Hestia.Model;
+using Hestia.Model.Interfaces;
+using Hestia.WebService.ServiceModel;
+using LanguageExt;
+using Xunit;
+
+namespace Test.Hestia.WebService.ServiceModel
+{
+    public class SlimSnapshotTest
+    {
+        [Fact]
+        public void FromCreatesCorrectSlimSnapshotRepresentation()
+        {
+            var snapshot = new RepositorySnapshot("bla",
+                                                  new List<IFile>(),
+                                                  Option<string>.None,
+                                                  "123",
+                                                  DateTime.MinValue,
+                                                  "bla",
+                                                  "path").AsHeader();
+
+            var slim = SlimSnapshot.From(snapshot);
+
+            slim.Id
+                .Should()
+                .Be(snapshot.Id);
+            slim.Name
+                .Should()
+                .Be(snapshot.Name);
+            slim.AtHash
+                .Should()
+                .Be(snapshot.AtHash);
+            slim.CommitDate
+                .Should()
+                .Be(snapshot.CommitDate);
+        }
+    }
+}
