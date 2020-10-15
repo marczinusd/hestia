4fa18df45509b02afb2797a8e7fe853fb447d18c
David Marczinus <marczinus.david@gmail.com>
2020-10-15 19:14:17 +0200

    refactor: clean up DBSetup

--- src/Hestia.UIRunner/DBSetup.cs
+++ src/Hestia.UIRunner/DBSetup.cs
@@ -10,22 +10,33 @@ namespace Hestia.UIRunner
     [ExcludeFromCodeCoverage]
     internal static class DbSetup
     {
+        private const string DBPathConfigKey = "dbPath";
+        private const string DBNameConfigKey = "dbName";
+        private const string DefaultDBName = "hestia.db";
+
+        private static readonly string DefaultDBFolder =
+            Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dev");
+
         public static readonly Lazy<HestiaContext> Context = new Lazy<HestiaContext>(() =>
         {
-            var dbPathFromConfig = ConfigurationManager.AppSettings["dbPath"];
-            var dbNameFromConfig = ConfigurationManager.AppSettings["dbName"];
-            var dbFolder = string.IsNullOrWhiteSpace(dbPathFromConfig)
-                               ? Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "dev")
-                               : dbPathFromConfig;
-            var dbName = string.IsNullOrWhiteSpace(dbNameFromConfig) ? "hestia.db" : dbNameFromConfig;
-
+            var dbPath = BuildDBPath();
             var contextBuilder = new DbContextOptionsBuilder();
-            contextBuilder.UseSqlite($@"Data Source={Path.Join(dbFolder, dbName)}");
+            contextBuilder.UseSqlite($@"Data Source={dbPath}");
             var dbContext = new HestiaContext(contextBuilder.Options);
-            EnsureDBCreated(dbContext, dbFolder);
+            EnsureDBCreated(dbContext, Path.GetDirectoryName(dbPath));
 
             return dbContext;
         });
+
+        private static string BuildDBPath()
+        {
+            var dbPathFromConfig = ConfigurationManager.AppSettings[DBPathConfigKey];
+            var dbNameFromConfig = ConfigurationManager.AppSettings[DBNameConfigKey];
+            var dbFolder = string.IsNullOrWhiteSpace(dbPathFromConfig) ? DefaultDBFolder : dbPathFromConfig;
+            var dbName = string.IsNullOrWhiteSpace(dbNameFromConfig) ? DefaultDBName : dbNameFromConfig;
+
+            return Path.Join(dbFolder, dbName);
+        }
 
         private static void EnsureDBCreated(HestiaContext context, string dbFolder)
         {
